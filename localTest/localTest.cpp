516763846145def964eb965fdda3b699cd7610d7
NeoWang9999@gmail.com <NeoWang9999@gmail.com>
2021-12-20 21:54:43 +0800

    first commit

--- localTest/localTest.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include<iostream>
-#include<vector>
-#include<numeric>
-using namespace std;
-int main() {
-    vector<int> v = { 2,7,6,10 };
-    cout << "Sum of all the elements are:" << endl;
-    cout << accumulate(v.begin(), v.end(), 2);
-}
