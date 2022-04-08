d16c4b1f2256202f986da64e4e04c88081a218f3
NeoWang9999@gmail.com <NeoWang9999@gmail.com>
2022-04-08 16:03:29 +0800

    fix> homework1 bug

--- Games101_homework1/main.cpp
+++ Games101_homework1/main.cpp
@@ -53,7 +53,7 @@ Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
     // Then return it.
     float eye_fov_radian = DEG2RAD(eye_fov);
 
-    float t = -std::tan(eye_fov_radian / 2) * std::abs(zNear);
+    float t = std::tan(eye_fov_radian / 2) * std::abs(zNear);
     float r = t * aspect_ratio;
     float l = -r;
     float b = -t;
@@ -124,7 +124,7 @@ int main(int argc, const char** argv)
 
         r.set_model(get_model_matrix(angle));
         r.set_view(get_view_matrix(eye_pos));
-        r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
+        r.set_projection(get_projection_matrix(45, 1, -0.1, -50));
 
         r.draw(pos_id, ind_id, rst::Primitive::Triangle);
         cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
@@ -141,7 +141,7 @@ int main(int argc, const char** argv)
         //r.set_model(get_model_matrix(angle));
         r.set_model(get_rotation(axis, angle));
         r.set_view(get_view_matrix(eye_pos));
-        r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
+        r.set_projection(get_projection_matrix(45, 1, -0.1, -50));
 
         r.draw(pos_id, ind_id, rst::Primitive::Triangle);
 
